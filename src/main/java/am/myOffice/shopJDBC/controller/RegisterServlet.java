50cc15599cbd154c51fea98e327773b8b5258519
NarekGharagyozyan <narekgharagyozyann@gmail.com>
2023-06-17 03:59:45 +0400

    some changes

--- src/main/java/am/myOffice/shopJDBC/controller/RegisterServlet.java
+++ src/main/java/am/myOffice/shopJDBC/controller/RegisterServlet.java
@@ -33,7 +33,44 @@ public class RegisterServlet extends HttpServlet {
             userService.register(user);
             resp.sendRedirect("home.html");
         } catch (Exception e) {
-            resp.getWriter().write("Go back and try again");
+            resp.getWriter().write("<!doctype html>\n" +
+                                   "<html lang=\"en\">\n" +
+                                   "<head>\n" +
+                                   "    <meta charset=\"UTF-8\">\n" +
+                                   "    <meta name=\"viewport\"\n" +
+                                   "          content=\"width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0\">\n" +
+                                   "    <meta http-equiv=\"X-UA-Compatible\" content=\"ie=edge\">\n" +
+                                   "    <title>Document</title>\n" +
+                                   "    <style>\n" +
+                                   "        input {\n" +
+                                   "            padding: 10px ;\n" +
+                                   "            margin-bottom: 10px;\n" +
+                                   "        }\n" +
+                                   "\n" +
+                                   "        .button {\n" +
+                                   "            padding: 10px 30px;\n" +
+                                   "        }\n" +
+                                   "\n" +
+                                   "    </style>\n" +
+                                   "</head>\n" +
+                                   "<body>\n" +
+                                   "\n" +
+                                   "        <div class=\"lr\">\n" +
+                                   "            <h1>Please Register again</h1>\n" +
+                                   "            <form method=\"post\" action=\"/register\">\n" +
+                                   "                <input type=\"text\" placeholder=\"name\" name=\"name\">\n" +
+                                   "                <input type=\"text\" placeholder=\"lastname\" name=\"lastname\"></br>\n" +
+                                   "                <input type=\"text\" placeholder=\"balance\" name=\"balance\">\n" +
+                                   "                <input type=\"email\" placeholder=\"email\" name=\"email\"></br>\n" +
+                                   "                <input type=\"password\" placeholder=\"password\" name=\"password\">\n" +
+                                   "                <input type=\"text\" placeholder=\"age\" name=\"age\"></br>\n" +
+                                   "                <input type=\"submit\" value=\"register\">\n" +
+                                   "            </form>\n" +
+                                   "        </div>\n" +
+                                   "    </div>\n" +
+                                   "\n" +
+                                   "</body>\n" +
+                                   "</html>");
         }
     }
 }
